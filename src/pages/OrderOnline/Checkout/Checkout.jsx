ccf3e73b741b5bc20bf9654fcb97d68d869ca18b
Kolya_cc <kolja12746853382gmail.com>
2025-03-18 18:22:01 +0200

    Finish-Frontend

--- src/pages/OrderOnline/Checkout/Checkout.jsx
+++ src/pages/OrderOnline/Checkout/Checkout.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import './Checkout.css';
 const Checkout = () => {
     return (
-        <div>
-            <div>
+        <div className='checkout'>
+            <div className='checkout-header'>
                 <NavLink to={"/OrderOnline"}>{'<'}</NavLink>
-                <h1>Checkout</h1>
+                <div><h1>Checkout</h1>
+                </div>
             </div>
             <h1>Shiping address</h1>
-            <div>
+            <div className='checkout-address'>
                
                 <input type="text" placeholder="Address"/>
                 <button>Change</button>
             </div>
             <h1>Order data</h1>
-            <div>
-                <div>
+            <div className='checkout-data'>
+                <div >
                     <input type="text" id="name" placeholder="First name"/>
                     <input type="text" id="name" placeholder="Last name"/>
                     <input type="text" id="name" placeholder="Phone number"/>
@@ -24,41 +25,47 @@ const Checkout = () => {
                 </div>
                 <div>
                     
-                    <input type="email" id="email" placeholder="Note"/>
+                    <input type="text" id="email" placeholder="Note"/>
                 </div>
             </div>
-            <h1>Order time</h1>
-            <div>
-                <input type="radio" id="contactChoice1" name="contact"  />
-                <label for="contactChoice1">Order now</label>
+            <div className='custom-checkbox' id='checkout-checkbox'>   
+                <h1>Order time</h1>
+                <div>
+                    <input type="radio" id="contactChoice1" name="contact"  />
+                    <label for="contactChoice1">Order now</label>
 
-                <input type="radio" id="contactChoice2" name="contact"  />
-                <label for="contactChoice2">Order later</label>
+                    <input type="radio" id="contactChoice2" name="contact"  />
+                    <label for="contactChoice2">Order later</label>
+                </div>
+                <h1>Order method</h1>
+                <div>
+                    <input type="radio" id="contactChoice3" name="contact2"  />
+                    <label for="contactChoice3">Delivery</label>
+
+                    <input type="radio" id="contactChoice4" name="contact2"  />
+                    <label for="contactChoice4">Take a way</label>
+                </div>
+                <h1>Payment method</h1>
+                <div className='payment-method'>
+                    <input type="radio" id="Payment1" name="contact3"  />
+                    <label for="Payment1">Cash On Delivery</label>
+
+                    <input type="radio" id="Payment2" name="contact3"  />
+                    <label for="Payment2">BCA Virtual Account</label>
+
+                    <input type="radio" id="Payment3" name="contact3"  />
+                    <label for="Payment3">Credit Card</label>
+
+                    <input type="radio" id="Payment4" name="contact3"  />
+                    <label for="Payment4">Transfer Bank</label>
+                </div>
             </div>
-            <div>
-                <input type="radio" id="contactChoice3" name="contact2"  />
-                <label for="contactChoice3">Delivery</label>
-
-                <input type="radio" id="contactChoice4" name="contact2"  />
-                <label for="contactChoice4">Take a way</label>
+            <div className='custom-checkbox' id='checkout-checkbox-2'>
+                <input   type="checkbox" id="checkbox1"/>
+                <label for="checkbox1">Choose to indicate that you have read and agree to our Terms of use & Privacy Policy.</label>
             </div>
-            <h1>Payment method</h1>
-            <div>
-                <input type="radio" id="Payment1" name="contact3"  />
-                <label for="Payment1">Cash On Delivery</label>
-
-                <input type="radio" id="Payment2" name="contact3"  />
-                <label for="Payment2">BCA Virtual Account</label>
-
-                <input type="radio" id="Payment3" name="contact3"  />
-                <label for="Payment3">Credit Card</label>
-
-                <input type="radio" id="Payment4" name="contact3"  />
-                <label for="Payment4">Transfer Bank</label>
-            </div>
-            <input type="checkbox" id="checkbox1"/>
-            <label for="checkbox1">Choose to indicate that you have read and agree to our Terms of use & Privacy Policy.</label>
-            <button>Order now</button>
+            <div className='Order-Now'><button>Order now</button></div>
+            
         </div>
     );
 };
--- src/pages/OrderOnline/OrderOnline.jsx
+++ src/pages/OrderOnline/OrderOnline.jsx
@@ -1,31 +1,59 @@
 import React from 'react';
 import Menu from '../menu/Menu.jsx'
+import './OrderOnline.css';
 import { NavLink } from 'react-router-dom';
+import { Dishes } from '../menu/Dish/Dish'
+import Dish from '../menu/Dish/Dish.jsx'
 const OrderOnline = () => {
     return (
-        <div>
-            <h1>Menu</h1>
-            <Menu />
-            <div>
-                <div><h1>Order list</h1></div>
+        <div className='OrderOnline'>
+            <div className='container-Menu'><h1>Menu</h1></div>
+            <div className='menu-category'>
+                <button>All category</button>
+                <button>Dinner</button>
+                <button>Launch</button>
+                <button>Dessert</button>
+                <button>Drink</button>
+            </div>
+            <div className='OrderOnline-container'>
+            <div className='dish-containerOrder' id='OrderOnline'>
+                {Dishes.map((dish)=> (
+                <Dish 
+                    key = {dish.id}
+                    title = {dish.title} 
+                    rating = {dish.rating}
+                    description = {dish.description} 
+                    price = {dish.price}
+                    DishImg ={dish.dishImg} 
+                />
+                ))}
+            </div>
+            <div className='Order-list-container'>
+                <div className='Order-list'><h1>Order list</h1></div>
                 <div>
-                    <div>Item 1</div>
-                    <div>Item 2</div>
+                    <h2>Item 1</h2>
+                    <h2>Item 2</h2>
                     
                 </div>
-                <div>
-                    <h1>Voucher code</h1>
-                    <input type="text" id="name" placeholder="Voucher code"/>
-                    <button>+</button>
+                <div className='Order-list-voucher'>
+                    <h2>Voucher code</h2>
+                    <div>
+                        <input type="text" id="name" placeholder="Voucher code"/>
+                        <button>+</button>
+                    </div>
+                    
                 </div>
-                <div>
-                    <h2>Subtotal</h2>
-                    <h2>Tax fee</h2>
-                    <h2>Voucher</h2>
-                    <h2>Total</h2>
-                    <NavLink to={"/OrderOnline/Checkout"}>Checkout</NavLink>
+                <div className='Order-list-total'>
+                    <div><h2>Subtotal</h2> <h3>55$</h3></div>
+                    <div><h2>Tax fee</h2> <h3>55$</h3></div>
+                    <div><h2>Voucher</h2> <h3>55$</h3></div>
+                    <div><h2>Total</h2> <h3>55$</h3></div>
+                    
+                    
+                    <NavLink id='Checkout' to={"/OrderOnline/Checkout"}>Checkout</NavLink>
                 </div>
                     
+            </div>
             </div>
         </div>
     );
