42c8e8f501c0a86b4cf4bb7c13f109f0e076b452
Kolya_cc <kolja12746853382gmail.com>
2025-05-24 15:01:15 +0300

    Remove src/settings/setings.ts from repo

--- Backend/src/settings/setings.ts
+++
@@ -1,9 +0,0 @@
-export const settings = {
-    MONGO_URI: process.env.MONGOURL || 'mongodb://localhost:27017',
-    JWT_SECRET: process.env.JWT_SECRET || 'abs',
-    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || 'abcd',
-    Email: process.env.EMAIL || 'deliziosorestoran@gmail.com',
-    Password: process.env.PASSWORD || 'drpqzdprktnvbsww',
-    CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:5173',
-    API_URL: process.env.API_URL || 'localhost:3001',
-}
